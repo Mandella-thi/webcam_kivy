175fd556956229c61cedf3b6d10efee382942e63
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2023-02-02 17:34:51 -0300

    "Webcamera que captura sua imagem"

---
+++ main.py
@@ -0,0 +1,37 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from FileSharer import FileSharer
+import cv2
+import time
+
+
+
+Builder.load_file('FrontEnd.kv')
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play =True
+        self.ids.camera_button.text ="Stop Camera"
+
+    def stop(self):
+        self.ids.camera.play = False
+        self.ids.camera_button.text = "Start Camera"
+        self.ids.camera.texture = None
+    def capture(self):
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        filepath = f'file/{current_time}.png'
+        self.ids.camera.export_to_png(filepath)
+
+class ImageScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
